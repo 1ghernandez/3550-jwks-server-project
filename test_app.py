9f39882497d785a5ace0aaaf554d1607b21ae4a2
Gloria Hernandez <gloriahernandez5@my.unt.edu>
2024-03-02 14:47:33 -0600

    Added  comments.

--- test_app.py
+++ test_app.py
@@ -1,12 +1,15 @@
 # tests/test_app.py
+#test suite required by assignment
 import pytest
 from app import app, keysStorage 
 
+# creating a reusable test component that can be used as arguments in test functions
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+# tests the endpoint of client
 def test_jwks_endpoint(client):
     """Test the JWKS endpoint for a successful response."""
     rv = client.get('/.well-known/jwks.json')
@@ -14,12 +17,14 @@ def test_jwks_endpoint(client):
     assert 'keys' in rv.json
     assert len(rv.json['keys']) > 0 
 
+# tests the authentication endpoint of client
 def test_auth_endpoint_success(client):
     """Test the auth endpoint for a successful token generation."""
     rv = client.post('/auth')
     assert rv.status_code == 200
     assert 'token' in rv.json
 
+# tests the expiry authentication endpoint
 def test_auth_endpoint_with_expired(client):
     """Test the auth endpoint to ensure it handles expired keys correctly."""
     rv = client.post('/auth?expired=true')
